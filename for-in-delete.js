648eebdb18ab341a14e2ab44ec28168539dd17ac
wsdodson <wsdodson@gmail.com>
2018-01-24 18:46:38 -0700

    finished

--- for-in-delete.js
+++ for-in-delete.js
@@ -41,6 +41,11 @@
 
 function showValues( obj ) {
   //Code Here
+  for(var key in obj){
+    let str = ""
+    str += obj[key]
+  }
+  return str
 }
 
 
@@ -54,7 +59,14 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function greaterThan10 (obj) {
+  for (let prop in obj) {
+    if (obj[prop] > 10) {
+      obj[prop] = 0
+    }
+  }
+  return obj
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -66,7 +78,12 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function double(obj){
+  for(let prop in obj){
+    obj[prop]*=2
+  }
+  return obj
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -80,6 +97,15 @@ function showValues( obj ) {
 */
 
 //Code Here
+function secrets (obj) {
+  var str = ""
+  for (let prop in obj) {
+    if (prop[0] === 's' && prop[1] === 'h') {
+      str += obj[prop]
+    }
+  }
+  return str
+}
 
 
 
@@ -112,7 +138,11 @@ function showValues( obj ) {
 
 //Code Here
 
-
+function removePassword(obj){
+  
+  delete obj.password
+  return obj
+}
 
 ////////// PROBLEM 6 //////////
 
@@ -130,7 +160,11 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
-
+for (let prop in deleteTheBigNumbers) {
+  if (deleteTheBigNumbers[prop] > 100) {
+    delete deleteTheBigNumbers[prop]
+  }
+}
 
 
 ////////// PROBLEM 7 //////////
@@ -158,5 +192,12 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
-
+function hiddenTreasure (obj) {
+  for (let prop in obj) {
+    if (!obj[prop].includes('treasure')) {
+      delete obj[prop]
+    }
+  }
+  return obj
+}
 
